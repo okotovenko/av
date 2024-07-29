bf56edaba71914cbebbdcc0b477e9e179d9181c8
okotovenko <oakotovenko@ukr.net>
2024-07-29 23:11:42 +0300

    6

--- dist/js/app.js
+++ dist/js/app.js
@@ -3861,231 +3861,6 @@
         }));
     }));
     Swiper.use([ Resize, Observer ]);
-    function Mousewheel(_ref) {
-        let {swiper, extendParams, on, emit} = _ref;
-        const window = ssr_window_esm_getWindow();
-        extendParams({
-            mousewheel: {
-                enabled: false,
-                releaseOnEdges: false,
-                invert: false,
-                forceToAxis: false,
-                sensitivity: 1,
-                eventsTarget: "container",
-                thresholdDelta: null,
-                thresholdTime: null,
-                noMousewheelClass: "swiper-no-mousewheel"
-            }
-        });
-        swiper.mousewheel = {
-            enabled: false
-        };
-        let timeout;
-        let lastScrollTime = utils_now();
-        let lastEventBeforeSnap;
-        const recentWheelEvents = [];
-        function normalize(e) {
-            const PIXEL_STEP = 10;
-            const LINE_HEIGHT = 40;
-            const PAGE_HEIGHT = 800;
-            let sX = 0;
-            let sY = 0;
-            let pX = 0;
-            let pY = 0;
-            if ("detail" in e) sY = e.detail;
-            if ("wheelDelta" in e) sY = -e.wheelDelta / 120;
-            if ("wheelDeltaY" in e) sY = -e.wheelDeltaY / 120;
-            if ("wheelDeltaX" in e) sX = -e.wheelDeltaX / 120;
-            if ("axis" in e && e.axis === e.HORIZONTAL_AXIS) {
-                sX = sY;
-                sY = 0;
-            }
-            pX = sX * PIXEL_STEP;
-            pY = sY * PIXEL_STEP;
-            if ("deltaY" in e) pY = e.deltaY;
-            if ("deltaX" in e) pX = e.deltaX;
-            if (e.shiftKey && !pX) {
-                pX = pY;
-                pY = 0;
-            }
-            if ((pX || pY) && e.deltaMode) if (e.deltaMode === 1) {
-                pX *= LINE_HEIGHT;
-                pY *= LINE_HEIGHT;
-            } else {
-                pX *= PAGE_HEIGHT;
-                pY *= PAGE_HEIGHT;
-            }
-            if (pX && !sX) sX = pX < 1 ? -1 : 1;
-            if (pY && !sY) sY = pY < 1 ? -1 : 1;
-            return {
-                spinX: sX,
-                spinY: sY,
-                pixelX: pX,
-                pixelY: pY
-            };
-        }
-        function handleMouseEnter() {
-            if (!swiper.enabled) return;
-            swiper.mouseEntered = true;
-        }
-        function handleMouseLeave() {
-            if (!swiper.enabled) return;
-            swiper.mouseEntered = false;
-        }
-        function animateSlider(newEvent) {
-            if (swiper.params.mousewheel.thresholdDelta && newEvent.delta < swiper.params.mousewheel.thresholdDelta) return false;
-            if (swiper.params.mousewheel.thresholdTime && utils_now() - lastScrollTime < swiper.params.mousewheel.thresholdTime) return false;
-            if (newEvent.delta >= 6 && utils_now() - lastScrollTime < 60) return true;
-            if (newEvent.direction < 0) {
-                if ((!swiper.isEnd || swiper.params.loop) && !swiper.animating) {
-                    swiper.slideNext();
-                    emit("scroll", newEvent.raw);
-                }
-            } else if ((!swiper.isBeginning || swiper.params.loop) && !swiper.animating) {
-                swiper.slidePrev();
-                emit("scroll", newEvent.raw);
-            }
-            lastScrollTime = (new window.Date).getTime();
-            return false;
-        }
-        function releaseScroll(newEvent) {
-            const params = swiper.params.mousewheel;
-            if (newEvent.direction < 0) {
-                if (swiper.isEnd && !swiper.params.loop && params.releaseOnEdges) return true;
-            } else if (swiper.isBeginning && !swiper.params.loop && params.releaseOnEdges) return true;
-            return false;
-        }
-        function handle(event) {
-            let e = event;
-            let disableParentSwiper = true;
-            if (!swiper.enabled) return;
-            if (event.target.closest(`.${swiper.params.mousewheel.noMousewheelClass}`)) return;
-            const params = swiper.params.mousewheel;
-            if (swiper.params.cssMode) e.preventDefault();
-            let targetEl = swiper.el;
-            if (swiper.params.mousewheel.eventsTarget !== "container") targetEl = document.querySelector(swiper.params.mousewheel.eventsTarget);
-            const targetElContainsTarget = targetEl && targetEl.contains(e.target);
-            if (!swiper.mouseEntered && !targetElContainsTarget && !params.releaseOnEdges) return true;
-            if (e.originalEvent) e = e.originalEvent;
-            let delta = 0;
-            const rtlFactor = swiper.rtlTranslate ? -1 : 1;
-            const data = normalize(e);
-            if (params.forceToAxis) if (swiper.isHorizontal()) if (Math.abs(data.pixelX) > Math.abs(data.pixelY)) delta = -data.pixelX * rtlFactor; else return true; else if (Math.abs(data.pixelY) > Math.abs(data.pixelX)) delta = -data.pixelY; else return true; else delta = Math.abs(data.pixelX) > Math.abs(data.pixelY) ? -data.pixelX * rtlFactor : -data.pixelY;
-            if (delta === 0) return true;
-            if (params.invert) delta = -delta;
-            let positions = swiper.getTranslate() + delta * params.sensitivity;
-            if (positions >= swiper.minTranslate()) positions = swiper.minTranslate();
-            if (positions <= swiper.maxTranslate()) positions = swiper.maxTranslate();
-            disableParentSwiper = swiper.params.loop ? true : !(positions === swiper.minTranslate() || positions === swiper.maxTranslate());
-            if (disableParentSwiper && swiper.params.nested) e.stopPropagation();
-            if (!swiper.params.freeMode || !swiper.params.freeMode.enabled) {
-                const newEvent = {
-                    time: utils_now(),
-                    delta: Math.abs(delta),
-                    direction: Math.sign(delta),
-                    raw: event
-                };
-                if (recentWheelEvents.length >= 2) recentWheelEvents.shift();
-                const prevEvent = recentWheelEvents.length ? recentWheelEvents[recentWheelEvents.length - 1] : void 0;
-                recentWheelEvents.push(newEvent);
-                if (prevEvent) {
-                    if (newEvent.direction !== prevEvent.direction || newEvent.delta > prevEvent.delta || newEvent.time > prevEvent.time + 150) animateSlider(newEvent);
-                } else animateSlider(newEvent);
-                if (releaseScroll(newEvent)) return true;
-            } else {
-                const newEvent = {
-                    time: utils_now(),
-                    delta: Math.abs(delta),
-                    direction: Math.sign(delta)
-                };
-                const ignoreWheelEvents = lastEventBeforeSnap && newEvent.time < lastEventBeforeSnap.time + 500 && newEvent.delta <= lastEventBeforeSnap.delta && newEvent.direction === lastEventBeforeSnap.direction;
-                if (!ignoreWheelEvents) {
-                    lastEventBeforeSnap = void 0;
-                    let position = swiper.getTranslate() + delta * params.sensitivity;
-                    const wasBeginning = swiper.isBeginning;
-                    const wasEnd = swiper.isEnd;
-                    if (position >= swiper.minTranslate()) position = swiper.minTranslate();
-                    if (position <= swiper.maxTranslate()) position = swiper.maxTranslate();
-                    swiper.setTransition(0);
-                    swiper.setTranslate(position);
-                    swiper.updateProgress();
-                    swiper.updateActiveIndex();
-                    swiper.updateSlidesClasses();
-                    if (!wasBeginning && swiper.isBeginning || !wasEnd && swiper.isEnd) swiper.updateSlidesClasses();
-                    if (swiper.params.loop) swiper.loopFix({
-                        direction: newEvent.direction < 0 ? "next" : "prev",
-                        byMousewheel: true
-                    });
-                    if (swiper.params.freeMode.sticky) {
-                        clearTimeout(timeout);
-                        timeout = void 0;
-                        if (recentWheelEvents.length >= 15) recentWheelEvents.shift();
-                        const prevEvent = recentWheelEvents.length ? recentWheelEvents[recentWheelEvents.length - 1] : void 0;
-                        const firstEvent = recentWheelEvents[0];
-                        recentWheelEvents.push(newEvent);
-                        if (prevEvent && (newEvent.delta > prevEvent.delta || newEvent.direction !== prevEvent.direction)) recentWheelEvents.splice(0); else if (recentWheelEvents.length >= 15 && newEvent.time - firstEvent.time < 500 && firstEvent.delta - newEvent.delta >= 1 && newEvent.delta <= 6) {
-                            const snapToThreshold = delta > 0 ? .8 : .2;
-                            lastEventBeforeSnap = newEvent;
-                            recentWheelEvents.splice(0);
-                            timeout = utils_nextTick((() => {
-                                swiper.slideToClosest(swiper.params.speed, true, void 0, snapToThreshold);
-                            }), 0);
-                        }
-                        if (!timeout) timeout = utils_nextTick((() => {
-                            const snapToThreshold = .5;
-                            lastEventBeforeSnap = newEvent;
-                            recentWheelEvents.splice(0);
-                            swiper.slideToClosest(swiper.params.speed, true, void 0, snapToThreshold);
-                        }), 500);
-                    }
-                    if (!ignoreWheelEvents) emit("scroll", e);
-                    if (swiper.params.autoplay && swiper.params.autoplayDisableOnInteraction) swiper.autoplay.stop();
-                    if (params.releaseOnEdges && (position === swiper.minTranslate() || position === swiper.maxTranslate())) return true;
-                }
-            }
-            if (e.preventDefault) e.preventDefault(); else e.returnValue = false;
-            return false;
-        }
-        function events(method) {
-            let targetEl = swiper.el;
-            if (swiper.params.mousewheel.eventsTarget !== "container") targetEl = document.querySelector(swiper.params.mousewheel.eventsTarget);
-            targetEl[method]("mouseenter", handleMouseEnter);
-            targetEl[method]("mouseleave", handleMouseLeave);
-            targetEl[method]("wheel", handle);
-        }
-        function enable() {
-            if (swiper.params.cssMode) {
-                swiper.wrapperEl.removeEventListener("wheel", handle);
-                return true;
-            }
-            if (swiper.mousewheel.enabled) return false;
-            events("addEventListener");
-            swiper.mousewheel.enabled = true;
-            return true;
-        }
-        function disable() {
-            if (swiper.params.cssMode) {
-                swiper.wrapperEl.addEventListener(event, handle);
-                return true;
-            }
-            if (!swiper.mousewheel.enabled) return false;
-            events("removeEventListener");
-            swiper.mousewheel.enabled = false;
-            return true;
-        }
-        on("init", (() => {
-            if (!swiper.params.mousewheel.enabled && swiper.params.cssMode) disable();
-            if (swiper.params.mousewheel.enabled) enable();
-        }));
-        on("destroy", (() => {
-            if (swiper.params.cssMode) enable();
-            if (swiper.mousewheel.enabled) disable();
-        }));
-        Object.assign(swiper.mousewheel, {
-            enable,
-            disable
-        });
-    }
     function create_element_if_not_defined_createElementIfNotDefined(swiper, originalParams, params, checkProps) {
         if (swiper.params.createElements) Object.keys(checkProps).forEach((key => {
             if (!params[key] && params.auto === true) {
@@ -4806,13 +4581,13 @@
             },
             on: {}
         });
-        //! Робичий варіант без position: fixed========================================================================== 
+        //! Робочий варіант
                 if (document.querySelector(".services__slider")) {
             const isMobile = window.matchMedia("(max-width: 768px)").matches;
             const direction = isMobile ? "horizontal" : "vertical";
             const servicesSection = document.querySelector(".services");
             const swiper = new Swiper(".services__slider", {
-                modules: [ Navigation, Scrollbar ],
+                modules: [ Navigation, Scrollbar, Autoplay ],
                 direction,
                 slidesPerView: 1,
                 navigation: {
@@ -4825,6 +4600,12 @@
                     horizontalClass: "swiper-scrollbar-horizontal",
                     verticalClass: "swiper-scrollbar-vertical"
                 },
+                autoplay: isMobile ? {
+                    delay: 5e3,
+                    disableOnInteraction: false
+                } : false,
+                loop: isMobile ? true : false,
+                speed: 800,
                 on: {
                     init: function() {
                         updateScrollbar(this);
@@ -4884,12 +4665,14 @@
             const observer = new IntersectionObserver((entries => {
                 entries.forEach((entry => {
                     if (entry.target === servicesSection) if (entry.isIntersecting) {
-                        servicesSection.classList.add("_watcher-view");
-                        additionalScrolls = 0;
-                        document.body.classList.add("no-scroll");
-                        document.addEventListener("wheel", handleWheelEvent, {
-                            passive: false
-                        });
+                        if (!isMobile) {
+                            servicesSection.classList.add("_watcher-view");
+                            additionalScrolls = 0;
+                            document.body.classList.add("no-scroll");
+                            document.addEventListener("wheel", handleWheelEvent, {
+                                passive: false
+                            });
+                        }
                     } else {
                         servicesSection.classList.remove("_watcher-view");
                         document.body.classList.remove("no-scroll");
@@ -4906,44 +4689,22 @@
         function initializeProcessSlider() {
             const isMobile = window.matchMedia("(max-width: 768px)").matches;
             let processSwiper = null;
-            if (isMobile) {
-                if (!document.querySelector(".process__slider").swiper) processSwiper = new Swiper(".process__slider", {
-                    modules: [ Navigation, Mousewheel, Scrollbar ],
-                    direction: "vertical",
-                    slidesPerView: 1,
-                    mousewheel: {
-                        invert: false
-                    },
-                    scrollbar: {
-                        el: ".process-scrollbar",
-                        draggable: true,
-                        verticalClass: "swiper-scrollbar-vertical"
-                    },
-                    on: {
-                        init: function() {
-                            updateScrollbar(this);
-                        },
-                        slideChange: function() {
-                            updateScrollbar(this);
-                        }
-                    }
-                });
-            } else if (processSwiper) {
-                processSwiper.destroy(true, true);
-                processSwiper = null;
-            }
-            function updateScrollbar(swiperInstance) {
-                const totalSlides = swiperInstance.slides.length;
-                const currentIndex = swiperInstance.activeIndex;
-                const scrollbar = document.querySelector(".process-scrollbar");
-                const scrollbarHeight = 50;
-                const scrollbarTop = currentIndex / (totalSlides - 1) * (swiperInstance.height - scrollbarHeight);
-                scrollbar.style.top = `${scrollbarTop}px`;
-            }
+            if (isMobile) if (!document.querySelector(".process__slider").swiper) processSwiper = new Swiper(".process__slider", {
+                modules: [ Navigation ],
+                slidesPerView: 1,
+                navigation: {
+                    prevEl: ".process-button-prev",
+                    nextEl: ".process-button-next"
+                }
+            });
         }
+        initializeProcessSlider();
         window.addEventListener("resize", (function() {
             const isMobile = window.matchMedia("(max-width: 768px)").matches;
-            if (isMobile && !document.querySelector(".process__slider").swiper) initializeProcessSlider();
+            if (isMobile && !document.querySelector(".process__slider").swiper) {
+                console.log(123);
+                initializeProcessSlider();
+            }
         }));
         if (document.querySelector(".related-project__slider")) {
             const swiper = new Swiper(".related-project__slider", {
